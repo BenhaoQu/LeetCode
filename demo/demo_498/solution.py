e5192486c718f7fb4f1e2a6571aa0f8626863c2c
GitHub Action <action@github.com>
2025-08-24 16:16:10 +0000

    test: [20250825] Add (498)

---
+++ demo/demo_498/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.findDiagonalOrder(test_input)
+
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        pass
+
