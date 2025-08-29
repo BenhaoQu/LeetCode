42bce1354da538c750d87acd383d83259a98161f
GitHub Action <action@github.com>
2025-08-29 16:17:24 +0000

    test: [20250830] Add (36)

---
+++ demo/demo_36/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.isValidSudoku(test_input)
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        pass
+
