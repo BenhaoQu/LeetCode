c1defe282a78b1696bac455d5856a5ea108d1f95
GitHub Action <action@github.com>
2025-09-14 16:14:28 +0000

    test: [20250915] Add (1935)

---
+++ demo/demo_1935/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.canBeTypedWords(*test_input)
+
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        pass
+
