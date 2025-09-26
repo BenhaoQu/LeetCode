28f83315670eb40dbf046cb3a06666c1b92b94b9
GitHub Action <action@github.com>
2025-09-26 16:17:46 +0000

    test: [20250927] Add (812)

---
+++ demo/demo_812/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.largestTriangleArea(test_input)
+
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        pass
+
