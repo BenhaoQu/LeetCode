1c475b171239bcddf9c2cdd2f8ca5094f8b20afe
GitHub Action <action@github.com>
2025-11-10 16:20:47 +0000

    test: [20251111] Add (474)

---
+++ demo/demo_474/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.findMaxForm(*test_input)
+
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        pass
+
